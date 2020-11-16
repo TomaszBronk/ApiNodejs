43c2d8f8c234eebd16c44772dfe4242f9c32fece
TomaszB <tomaszbronk11@gmail.com>
2020-11-16 20:20:31 +0100

    body-parser

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@
 
 const PORT = 3000;
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 // GET /
 app.get('/', (req, res) => {
-res.send('Hello, world!') // default: 200, text/html
+res.send('Hello, world!'); // default: 200, text/html
+});
+
+app.post('/', (req, res) => {
+console.log(JSON.stringify(req.body, null, 2));
+res.json({ok: true});
 });
 
 app.get('/ping', (req, res) => {
@@ -16,9 +23,15 @@ res.json({status: 'ok'});
 //res.status(201).json({status: 'ok'});
 });
 
+app.listen(PORT), (err)=>{
+if (err){
+console.log.apply('Error!');
+throw err;
+} else{
+  console.log(`Node server is running on port ${PORT}`)
+}
 
-
-app.listen(PORT);
+};
 
 
 
