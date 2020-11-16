c696326ccf752e64a30ef8c93c6fe48effaee9e2
TomaszB <tomaszbronk11@gmail.com>
2020-11-16 20:10:37 +0100

    express intro

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-const http = require('http');
-const json5 = require('json5');
+//const http = require('http');
+//const json5 = require('json5');
 
 const PORT = 3000;
+const express = require('express');
 
-http.createServer(function(request, response){
+const app = express();
+
+// GET /
+app.get('/', (req, res) => {
+res.send('Hello, world!') // default: 200, text/html
+});
+
+app.get('/ping', (req, res) => {
+res.json({status: 'ok'});
+//res.status(201).json({status: 'ok'});
+});
+
+
+
+app.listen(PORT);
+
+
+
+/*http.createServer(function(request, response){
 const responseData = {
 status: 'ok!'
 
 };
-response.writeHead(201, {
+response.writeHead(200, {
   'Content-Type': 'application/json'
 });
 response.end(JSON.stringify(responseData));
 }).listen(PORT);
 
-console.log(`Node server is running on port ${PORT}`);
+console.log(`Node server is running on port ${PORT}`);*/
