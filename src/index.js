481c7eca43312e60ac7b4ff3d94d2fd23527ca01
TomaszB <tomaszbronk11@gmail.com>
2020-11-16 19:46:25 +0100

    starting app using nodemon

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const http = require('http');
+const json5 = require('json5');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const PORT = 3000;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+http.createServer(function(request, response){
+const responseData = {
+status: 'ok!'
+
+};
+response.writeHead(201, {
+  'Content-Type': 'application/json'
+});
+response.end(JSON.stringify(responseData));
+}).listen(PORT);
+
+console.log(`Node server is running on port ${PORT}`);
